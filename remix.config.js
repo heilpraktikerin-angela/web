723c14c352847468d864c0b5d44e9838afb866dd
benno <bennokohrs@gmail.com>
2022-10-23 16:53:16 +0200

    fixed typo

--- remix.config.js
+++ remix.config.js
@@ -9,6 +9,6 @@ module.exports = {
   devServerPort: 8002, // https://github.com/remix-run/remix/issues/2958
   appDirectory: './src/app',
   assetsBuildDirectory: 'public/build',
-  serverBuildPath: '.netlify/functions-internal/server.js',
+  // serverBuildPath: '.netlify/functions-internal/server.js',
   // publicPath: "/build/",
 };
