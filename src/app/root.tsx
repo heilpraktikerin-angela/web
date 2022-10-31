d9a1b4f64df62e3dc0ee1f24768e9a5189a59dcb
benno <bennokohrs@gmail.com>
2022-10-31 09:49:27 +0100

    removed google analytics tag for testing

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -61,30 +61,30 @@ const App: React.FC = () => {
   // (e.g. hide Navbar, different Navbar, ..)
   return (
     <RootContext.Provider value={ENV}>
-      {/* Google Analytics */}
-      {process.env.NODE_ENV === 'development' || !gaTrackingId ? null : (
-        <>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
-          />
-          <script
-            async
-            id="gtag-init"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
+      {/*/!* Google Analytics *!/*/}
+      {/*{process.env.NODE_ENV === 'development' || !gaTrackingId ? null : (*/}
+      {/*  <>*/}
+      {/*    <script*/}
+      {/*      async*/}
+      {/*      src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}*/}
+      {/*    />*/}
+      {/*    <script*/}
+      {/*      async*/}
+      {/*      id="gtag-init"*/}
+      {/*      dangerouslySetInnerHTML={{*/}
+      {/*        __html: `*/}
+      {/*          window.dataLayer = window.dataLayer || [];*/}
+      {/*          function gtag(){dataLayer.push(arguments);}*/}
+      {/*          gtag('js', new Date());*/}
 
-                gtag('config', '${gaTrackingId}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </>
-      )}
+      {/*          gtag('config', '${gaTrackingId}', {*/}
+      {/*            page_path: window.location.pathname,*/}
+      {/*          });*/}
+      {/*        `,*/}
+      {/*      }}*/}
+      {/*    />*/}
+      {/*  </>*/}
+      {/*)}*/}
 
       <Outlet context={ENV} />
     </RootContext.Provider>
