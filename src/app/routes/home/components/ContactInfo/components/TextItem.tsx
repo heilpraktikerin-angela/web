0ceba0adadeb401884fcaba2eab619354ad5f90f
benno <bennokohrs@gmail.com>
2022-11-01 10:21:39 +0100

    added mail deep link

--- src/app/routes/home/components/ContactInfo/components/TextItem.tsx
+++ src/app/routes/home/components/ContactInfo/components/TextItem.tsx
@@ -1,21 +1,36 @@
+import clsx from 'clsx';
 import React from 'react';
 import Icon from '~/components/icons';
 import { tss } from '~/core/helper/tss';
 import theme from '~/core/theme';
 
 const TextItem: React.FC<TTextItemProps> = (props) => {
-  const { text, icon } = props;
+  const { text, icon, href } = props;
 
   const IconComponent = Icon[icon];
 
-  return (
-    <div className={Container}>
+  const Content = () => (
+    <>
       <div className={IconContainer}>
         <IconComponent size={16} color={theme?.colors.secondary} />
       </div>
       <p className={Text}>{text}</p>
-    </div>
+    </>
   );
+
+  if (href != null) {
+    return (
+      <a className={clsx(Container, href && ClickableContainer)} href={href}>
+        <Content />
+      </a>
+    );
+  } else {
+    return (
+      <div className={clsx(Container, href && ClickableContainer)}>
+        <Content />
+      </div>
+    );
+  }
 };
 
 export default TextItem;
@@ -23,12 +38,17 @@ export default TextItem;
 type TTextItemProps = {
   text: string;
   icon: keyof typeof Icon;
+  href?: string;
 };
 
 const Container = tss`
   flex
   flex-row
   items-center
+`;
+
+const ClickableContainer = tss`
+  cursor-pointer
 `;
 
 const IconContainer = tss`
